da20a74c6a299d5a05be3dbad27843997bbdc7b1
isha2604 <65038913+isha2604@users.noreply.github.com>
2021-03-16 11:28:59 -0400

    Add files via upload

---
+++ file_1.R
@@ -0,0 +1,176 @@
+library(recommenderlab)
+library(ggplot2)
+library(data.table)
+library(reshape2)
+movie_data <- read.csv("C:/Users/ishas/Desktop/Movie Project__R/movies.csv", stringsAsFactors = FALSE)
+str(movie_data)
+ratings <- read.csv("C:/Users/ishas/Desktop/Movie Project__R/ratings.csv")
+str(ratings)
+View(ratings)
+summary(movie_data)
+head(movie_data)
+summary(ratings)
+head(ratings)
+movie_genre <- as.data.frame(movie_data$genres, stringsAsFactors = FALSE)
+movie_genre
+library(data.table)
+movie_genre2 <- as.data.frame(tstrsplit(movie_genre[,1], '[|]',type.convert=TRUE),stringsAsFactors=FALSE)
+movie_genre2
+colnames(movie_genre2) <- c(1:10)
+colnames
+list_genre <- c("Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western")
+list_genre
+genre_mat1 <- matrix(0,10330,18)
+genre_mat1
+genre_mat1 [1,] <- list_genre
+colnames(genre_mat1) <- list_genre
+colnames
+for (index in 1:nrow(movie_genre2)) {
+  for (col in 1:ncol(movie_genre2)) {
+    gen_col = which(genre_mat1[1,] == movie_genre2[index,col]) #Author DataFlair
+    genre_mat1[index+1,gen_col] <- 1
+  }
+}
+genre_mat2 <- as.data.frame(genre_mat1[-1,], stringsAsFactors=FALSE) #remove first row, which was the genre list
+for (col in 1:ncol(genre_mat2)) {
+  genre_mat2[,col] <- as.integer(genre_mat2[,col]) #convert from characters to integers
+} 
+str(genre_mat2)
+
+SearchMatrix <- cbind(movie_data[,1:2], genre_mat2[])
+head(SearchMatrix)
+
+ratingMatrix <- dcast(ratings, userId~movieId, value.var = "rating", na.rm=FALSE)
+ratingMatrix
+ratingMatrix <- as.matrix(ratingMatrix[,-1]) #remove userIds
+ratingMatrix
+ratingMatrix <- as(ratingMatrix, "realRatingMatrix")
+ratingMatrix
+
+recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+
+recommendation_model <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+names(recommendation_model)
+
+lapply(recommendation_model, "[[", "description")
+
+recommendation_model$IBCF_realRatingMatrix
+
+#Error from here
+similarity(ratingMatrix[1:4, ], method = "cosine", where = "users")
+similaritY_mat <- similarity(ratingMatrix[1:4, ], method = "cosine", which="users")
+as.matrix(similaritY_mat)
+image(as.matrix(similaritY_mat), main="Users Similarities")
+
+movie_similarity = similarity(ratingMatrix[,1:4], method = "cosine", which ="items")
+as.matrix(movie_similarity)
+movie_similarity
+image(as.matrix(movie_similarity), main="Movies Similarities")
+
+#extract the most unique ratings
+rating_values = as.vector(ratingMatrix@data)
+unique(rating_values)
+
+#Table of ratings
+Table_of_ratings <- table(rating_values)
+Table_of_ratings
+
+#most viewed movies visualization
+
+library(ggplot2)
+movie_views = colCounts(ratingMatrix)
+table_views = data.frame(movie = names(movie_views), views = movie_views)
+#create dataframe of views
+table_views = table_views[order(table_views$view , decreasing = TRUE),] #sort by number of views
+table_views$title = NA
+table_views
+for(index in 1:10325){
+  table_views[index,3] = as.character(subset(movie_data,movie_data$movieId == table_views[index,1])$title)
+}
+table_views[1:6,]
+dev.off()
+ggplot(table_views[1:6,], aes(x=title,y=views)) + 
+  geom_bar(stat="identity",fill="steelblue")+
+  geom_text(aes(label=views),vjust=-0.3,size=3.5)+
+  theme(axis.text.x = element_text(angle = 45,hjust = 1))+
+  ggtitle("Total Views of the Top Films")
+
+#heatmap of movie ratings
+image(ratingMatrix[1:25, 1:25], axes= FALSE, main ="HeatMap of first 25 rows and 25 columns")
+
+#Performing data preparation
+movie_ratings = ratingMatrix[rowCounts(ratingMatrix)>50 , colCounts(ratingMatrix)>50]
+movie_ratings
+
+minimum_movies =  quantile(rowCounts(movie_ratings),0.98)
+minimum_movies
+minimum_users  =  quantile(colCounts(movie_ratings),0.98)
+minimum_users
+
+image(movie_ratings[rowCounts(movie_ratings)>minimum_movies, 
+                    colCounts(movie_ratings)>minimum_users], main="HeatMap of top Users and Movies")
+
+#visualize average ratings per user
+average_ratings = rowMeans(movie_ratings)
+average_ratings
+qplot(average_ratings,fill=I("steelblue"),col=I("red"))+ggtitle("Distribution of the average ratings given by each users")
+
+#normalization
+normalized_ratings <- normalize(movie_ratings)
+normalized_ratings
+sum(rowMeans(normalized_ratings) > 0.00001)
+image(normalized_ratings[rowCounts(normalized_ratings) > minimum_movies,
+                         colCounts(normalized_ratings) > minimum_users],
+      main = "Normalized Ratings of the Top Users")
+
+#Data Binarization
+binary_minimum_movies <- quantile(rowCounts(movie_ratings), 0.95)
+binary_minimum_users <- quantile(colCounts(movie_ratings), 0.95)
+#movies_watched <- binarize(movie_ratings, minRating = 1)
+
+good_rated_films <- binarize(movie_ratings, minRating = 3)
+image(good_rated_films[rowCounts(movie_ratings) > binary_minimum_movies,
+                       colCounts(movie_ratings) > binary_minimum_users],
+main = "Heatmap of the top users and movies")
+
+#collaborative filtering system
+library(caret)
+library(e1071)
+sampled_data <- sample(x= c(TRUE, FALSE), size = nrow(movie_ratings), replace = TRUE, prob = c(0.8,0.2))
+training_data <- movie_ratings[sampled_data, ]
+testing_data <- movie_ratings[!sampled_data, ]
+
+recommendation_system = recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+recommendation_system$IBCF_realRatingMatrix$parameters
+
+recommen_model = Recommender(data=training_data,method="IBCF",parameter = list(k=30))
+recommen_model
+
+class(recommen_model)
+model_info <- getModel(recommen_model)
+class(model_info$sim)
+
+dim(model_info$sim)
+
+top_items= 20
+image(model_info$sim[1:top_items,1:top_items],main="Heatmap of first 20 rows and columns")
+
+sum_rows = rowSums(model_info$sim>0)
+table(sum_rows)
+sum_cols = colSums(model_info$sim>0)
+qplot(sum_cols,fill=I("steelblue"),col=I("red"))+ggtitle("Distribution of the column count")
+
+top_recommendations <- 10
+predicted_recommendations = predict(object=recommen_model,newdata=testing_data,n=top_recommendations)
+predicted_recommendations
+
+user1 = predicted_recommendations@items[[1]] #recommendation for the first user
+movies_user1=predicted_recommendations@itemLabels[user1]
+movies_user2 <- movies_user1
+for(index in 1:10){
+  movies_user2[index]=as.character(subset(movie_data,movie_data$movieId==movies_user1[index])$title)
+}
+movies_user2
+
+recommendation_matrix <- sapply(predicted_recommendations@items,function(x){as.integer(colnames(movie_ratings)[x])}) #matrix with movie recommendations for each user
+recommendation_matrix[,1:4]
